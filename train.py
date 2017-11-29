07cddc9b1f6264121816431a07e67768132dacc2
mohammed <hassoun@outlook.com>
2017-11-29 11:24:13 -0500

    init commit

---
+++ train.py
@@ -0,0 +1,102 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import glob
+import time
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from skimage.feature import hog
+from utils import *
+# NOTE: the next import is only valid for scikit-learn version <= 0.17
+# for scikit-learn >= 0.18 use:
+# from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
+import pickle
+
+# Read in cars and notcars
+cars = glob.glob('data/vehicles/*/*.png')
+notcars = glob.glob('data/non-vehicles/*/*.png')
+
+
+def data_look(car_list, notcar_list):
+    data_dict = {}
+    # Define a key in data_dict "n_cars" and store the number of car images
+    data_dict["n_cars"] = len(car_list)
+    # Define a key "n_notcars" and store the number of notcar images
+    data_dict["n_notcars"] = len(notcar_list)
+    # Read in a test image, either car or notcar
+    # Define a key "image_shape" and store the test image shape 3-tuple
+    example_img = mpimg.imread(car_list[0])
+    data_dict["image_shape"] = example_img.shape
+    # Define a key "data_type" and store the data type of the test image.
+    data_dict["data_type"] = example_img.dtype
+    # Return data_dict
+    return data_dict
+
+
+data_info = data_look(cars, notcars)
+
+print(data_info["n_cars"], ' cars and',
+      data_info["n_notcars"], ' non-cars')
+print('of size: ', data_info["image_shape"],
+      ' and data type:', data_info["data_type"])
+
+
+# Tweak these parameters and see how the results change.
+color_space = 'YCrCb'  # YUV LUV Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+orient = 9  # HOG orientations
+pix_per_cell = 8  # HOG pixels per cell
+cell_per_block = 2  # HOG cells per block
+hog_channel = "ALL"  # Can be 0, 1, 2, or "ALL"
+spatial_size = (16, 16)  # Spatial binning dimensions
+hist_bins = 16    # Number of histogram bins
+spatial_feat = True  # Spatial features on or off
+hist_feat = True  # Histogram features on or off
+hog_feat = True  # HOG features on or off
+y_start_stop = [400, None]  # Min and max in y to search in slide_window()
+
+car_features = extract_features(cars, color_space=color_space,
+                                spatial_size=spatial_size, hist_bins=hist_bins,
+                                orient=orient, pix_per_cell=pix_per_cell,
+                                cell_per_block=cell_per_block,
+                                hog_channel=hog_channel, spatial_feat=spatial_feat,
+                                hist_feat=hist_feat, hog_feat=hog_feat)
+notcar_features = extract_features(notcars, color_space=color_space,
+                                   spatial_size=spatial_size, hist_bins=hist_bins,
+                                   orient=orient, pix_per_cell=pix_per_cell,
+                                   cell_per_block=cell_per_block,
+                                   hog_channel=hog_channel, spatial_feat=spatial_feat,
+                                   hist_feat=hist_feat, hog_feat=hog_feat)
+
+X = np.vstack((car_features, notcar_features)).astype(np.float64)
+# Fit a per-column scaler
+X_scaler = StandardScaler().fit(X)
+# Apply the scaler to X
+scaled_X = X_scaler.transform(X)
+
+# Define the labels vector
+y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+
+# Split up data into randomized training and test sets
+rand_state = np.random.randint(0, 100)
+X_train, X_test, y_train, y_test = train_test_split(
+    scaled_X, y, test_size=0.2, random_state=rand_state)
+
+print('Using:', orient, 'orientations', pix_per_cell,
+      'pixels per cell and', cell_per_block, 'cells per block')
+print('Feature vector length:', len(X_train[0]))
+# Use a linear SVC
+model = LinearSVC()
+# Check the training time for the SVC
+t = time.time()
+model.fit(X_train, y_train)
+t2 = time.time()
+print(round(t2 - t, 2), 'Seconds to train model...')
+# Check the score of the SVC
+print('Test Accuracy of model = ', round(model.score(X_test, y_test), 4))
+
+# save model
+filename = 'model.pkl'
+pickle.dump(model, open(filename, 'wb'))
